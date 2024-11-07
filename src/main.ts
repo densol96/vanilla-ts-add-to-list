fa34c0920be9e82e6df23acc34159ec53338c726
Deniss Solovjovs <deniss11sol@gmail.com>
2024-11-07 21:44:47 +0200

    A little fix

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ const initApp = (): void => {
     const newEntryText: string = input.value.trim();
     if(!newEntryText.length) return;
     
-    const itemId = fullList.list.length ? parseInt(fullList.list[fullList.list.length - 1].id) + 1 : 1;
+    const itemId: number = fullList.list.length ? parseInt(fullList.list[fullList.list.length - 1].id) + 1 : 1;
 
     const newItem: ListItem = new ListItem(itemId.toString(), newEntryText);
     fullList.addItem(newItem);
